4c564ddd329aa1c369dcb03849a21321765c20cd
yuyuyu <y.imai@iimon.co.jp>
2024-08-26 18:49:36 +0900

    更地のreact

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// classを作成し、Componentを継承する
+class App extends Component {
+  render() {
+    return <p>hello world!</p>;
+  }
+}
+// export defaultを利用
+export default App;
